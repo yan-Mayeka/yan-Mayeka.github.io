49bf39675eeba61f28656aa1ff078da869fea794
yan-Mayeka <164897123+yan-Mayeka@users.noreply.github.com>
2025-09-19 21:59:36 +0200

    Create Font-Changer.js

---
+++ Font-Changer.js
@@ -0,0 +1,59 @@
+function oIlozellig() {
+  const paragraph = document.getElementById("IZ");
+
+  // You can either toggle between classes or set a specific new class
+  if (paragraph.className !== "Ilozellig") {
+    paragraph.className = "Ilozellig";
+  } else {
+    paragraph.className = "Ilozellig"; // Or another class if you have more
+  }
+
+function oLukazellig() {
+  const paragraph = document.getElementById("LZ");
+
+  // You can either toggle between classes or set a specific new class
+  if (paragraph.className !== "Lukazellig") {
+    paragraph.className = "Lukazellig";
+  } else {
+    paragraph.className = "Lukazellig"; // Or another class if you have more
+  }
+
+function oNSTAN() {
+  const paragraph = document.getElementById("NN");
+
+  // You can either toggle between classes or set a specific new class
+  if (paragraph.className !== "NSTAN") {
+    paragraph.className = "NSTAN";
+  } else {
+    paragraph.className = "NSTAN"; // Or another class if you have more
+  }
+
+function oConsolas() {
+  const paragraph = document.getElementById("CN");
+
+  // You can either toggle between classes or set a specific new class
+  if (paragraph.className !== "Consolas") {
+    paragraph.className = "Consolas";
+  } else {
+    paragraph.className = "Consolas"; // Or another class if you have more
+  }
+
+function oTowitela() {
+  const paragraph = document.getElementById("TW");
+
+  // You can either toggle between classes or set a specific new class
+  if (paragraph.className !== "Towitela") {
+    paragraph.className = "Towitela";
+  } else {
+    paragraph.className = "Towitela"; // Or another class if you have more
+  }
+
+function oAWT() {
+  const paragraph = document.getElementById("AW");
+
+  // You can either toggle between classes or set a specific new class
+  if (paragraph.className !== "Towitela") {
+    paragraph.className = "Towitela";
+  } else {
+    paragraph.className = "Towitela"; // Or another class if you have more
+  }
