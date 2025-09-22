7da5778f45b9bff296e96ccac34c6b5da050e1fa
yan-Mayeka <164897123+yan-Mayeka@users.noreply.github.com>
2025-09-22 05:51:54 +0200

    Update esun-sitelen.js

--- esun-sitelen.js
+++ esun-sitelen.js
@@ -25,49 +25,49 @@ terug.addEventListener('click', () => {
 if (PagNum == 0){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 0.png";
 }
-else if(PagNum == 1){
+ if(PagNum == 1){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 1.png";
 }
-else if(PagNum == 2){
+ if(PagNum == 2){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 2.png";
 }
-else if(PagNum == 3){
+ if(PagNum == 3){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 3.png";
 }
-else if(PagNum == 4){
+ if(PagNum == 4){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 4.png";
 }
-else if(PagNum == 5){
+ if(PagNum == 5){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 5.png";
 }
-else if(PagNum == 6){
+ if(PagNum == 6){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 6.png";
 }
-else if(PagNum == 7){
+ if(PagNum == 7){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 7.png";
 }
-else if(PagNum == 8){
+ if(PagNum == 8){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 8.png";
 }
-else if(PagNum == 9){
+ if(PagNum == 9){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 9.png";
 }
-else if(PagNum == 10){
+ if(PagNum == 10){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 10.png";
 }
-else if(PagNum == 11){
+ if(PagNum == 11){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 11.png";
 }
-else if(PagNum == 12){
+ if(PagNum == 12){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 12.png";
 }
-else if(PagNum == 13){
+ if(PagNum == 13){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 13.png";
 }
-else if(PagNum == 14){
+ if(PagNum == 14){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 14.png";
 }
-else if(PagNum == 15){
+ if(PagNum == 15){
   CurPag.src = "/assests/images/Maya's Sweet 16/MS16 lipu nanpa 15.png";
 }
 //Over and underflow
